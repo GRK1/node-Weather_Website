a346b677ff9af5f6d0cdbeed20a455734e347ae4
rahul <rahul46578@gmail.com>
2020-04-11 10:47:46 +0530

    Remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,26 @@
-
-console.log('client side javacript is loaded')
- 
-
-
-
-   const weatherform =document.querySelector('form')
-   const search = document.querySelector('input')
-   const message1 = document.querySelector('#message-1')
-   const message2  = document.querySelector('#message-2')
-   weatherform.addEventListener('submit',(e)=>{
-    e.preventDefault() 
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
     message1.textContent = 'Loading...'
-                message2.textContent=""  
-    fetch('http://localhost:3000/weather?address='+search.value).then((response)=>{
-        response.json().then((data)=>{
-            if(data.error)
-            {
+    message2.textContent = ""
+    fetch('http://localhost:3000/weather?address=' + search.value).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
                 console.log(data.error)
                 message1.textContent = data.error
-              
+
             }
-            else
-            {
-                message1.textContent=data.Location
-                message2.textContent= data.data
+            else {
+                message1.textContent = data.Location
+                message2.textContent = data.data
                 console.log(data.Location)
                 console.log(data.data)
             }
-            
+
         })
     })
-   })
+})
 
